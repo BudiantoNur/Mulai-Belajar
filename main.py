a63aaacdc33c8e0cc5e8d5119941d7ea2ea15b72
USER <nurarifb3@gmail.com>
2025-09-18 18:25:06 +0800

    data diri

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ nama = 'Arif'
 alamat = 'Bukit Kemuning'
 berat_badan= 80
 tinggi_badan= 168
-
-print('Nama =',nama)
-print('Alamat =',alamat)
-print('Berat Badan =',berat_badan)
-print('Tinggi badan=',tinggi_badan)
