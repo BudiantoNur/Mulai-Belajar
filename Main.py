fd9657d2c02d77ae88faf3c70c44f085e627207c
Budianto-Nur <145466695+BudiantoNur@users.noreply.github.com>
2025-10-01 13:02:52 +0000

    perubahan

--- Main.py
+++ Main.py
@@ -28,3 +28,18 @@ b = a
 print("Nilai b =", b)
 
 # komen apalagi ini
+# lanjut belajar tipe data
+
+# tipe data angka (integer)
+data_integer = 1
+print("data :", data_integer, ", bertipe :", type(data_integer))
+
+# tipe data float, angka dengan koma
+data_float = 1.5
+print("data_float : ",data_float, ", bertipe : ", type(data_float))
+
+# tipe data biner true / false (boolean)
+data_boolean = 1.5
+print("data_boolean)) : ",data_boolean)), ", bertipe : ", type(data_boolean))
+
+#tipe data khusus
