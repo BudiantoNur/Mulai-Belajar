f77887f8f83f4d83763d7b6b8e8bb263f07babae
Budianto-Nur <145466695+BudiantoNur@users.noreply.github.com>
2025-10-01 12:21:01 +0000

    perubahan

--- Main.py
+++ Main.py
@@ -1,11 +1,28 @@
-nama = 'Arif'
-alamat = 'Bukit Kemuning'
-berat_badan= 80
-tinggi_badan= 168
-pekerjaan= "Pekerja Swasta"
+# Belajar dari nol
 
-print("Nama=", nama)
-print("Alamat Rumah=", alamat)
-print("Berat Badan=", berat_badan)
-print("Tinggi Badan=", tinggi_badan)
-print("Perkaan=", pekerjaan)
+# variabel adalah tempat menyimpan data
+
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggila pertama
+print("Nilai a =", a)
+print("Nilai x =", x)
+print("Nilai panjang =", panjang)
+
+# penamaan
+nilay_y = 15 #dengan menggunakan underskor
+"""kalau seperti ini 10juta itu tidak boleh
+karna anggka tidak boleh di awal"""
+juta10 = 10000000 #ini boleh
+nilaiZ = 17.5 #ini boleh
+
+# pemanggilan kedua
+print("nilai a =", a)
+a = 7
+print("nilai a =", a)
+
+# assignment indirect
+b = a
+print("Nilai b =", b)
