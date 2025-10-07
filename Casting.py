346bfde1bd431828e992a04a0bffd677a35e1de3
Budianto-Nur <145466695+BudiantoNur@users.noreply.github.com>
2025-10-07 23:15:50 +0000

    casting

--- Casting.py
+++ Casting.py
@@ -4,7 +4,9 @@
 data_int = 9
 print("data =", data_int, "type = ", type(data_int))
 
-data_float = float(data_int)
 data_str = str(data_int)
-print("data =", data_float, "type = ", type(data_float))
-print("data =", data_str, "type = ", type(data_int))
+data_float = float(data_int)
+data_bool = bool(data_int)
+print("data = ", data_str, "type(data_str)")
+print("data = ", data_float, "type(data_float)")
+print("data = ", data_bool, "type(data_bool)")
